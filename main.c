fcd15451eee459a2e53a41b19c1dccb0f9e0dbf6
sanjuCo <145989871+sanjuCo@users.noreply.github.com>
2023-10-16 14:46:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int admissionNumber, age;
+
+    admissionNumber = 123;
+    age = 10;
+    char name[4] = "Sam";
+    char course[20] = "Computer Science";
+    char faculty[15] = "Computing";
+
+    printf("Name: %s\n", name);
+    printf("ADm/No: %d\n", admissionNumber);
+    printf("Age: %d\n", age);
+    printf("Course: %s\n", course);
+    printf("Faculty: %s", faculty);
+
+    return 0;
+}
